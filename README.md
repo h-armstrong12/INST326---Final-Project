fec2ec66f87ffa0c679920b2224c9bee8bc0284e
Everchanging16 <ms.protegee@gmail.com>
2024-05-09 16:10:27 -0400

    update

--- README.md
+++ README.md
@@ -20,3 +20,29 @@ Will Not Do:
 ● Stream music or video content directly within the application.
 ● Integrate with external databases or APIs for content fetching.
 
+Here is the specifics of how the program works:
+
+This program starts off by asking the user for a name, which will be referenced throughout the program.
+
+After a brief explanation of the program and how it works, the user is offered the option to either: select, recommend, alter, randomize.
+
+By choosing the select option, users will be able to get a list of songs from their genre of choice: Pop, Country, Rock, Classical, R&B, Soul, and HipHop and with that list of songs, be able to trim it based on what emotion they want to experience (users will be able to select from 14 unique emotions).
+By choosing the ‘recommend’ option, users will be recommended a list of songs based on the feelings they wish to experience, regardless of genre.
+When selecting the alter option, users will have the option to change the emotion assigned to a specific song if they think it’s necessary. The song text file was developed by our group along with the help of ChatGPT, and so some emotions assigned could not be fitting for certain songs. This option allows users to correct that mistake.
+And finally with the randomize option, users will be able to receive a completely randomized list of ten songs.
+Following the use of one of these options, users will have the option to select one of the four options again until they decide to end the program.
+
+When the user is expected to input something into the console, they are often told the options that are available to them to select, by typing otherwise will cause the user to face an issue or the end of the program. 
+
+
+
+Bibliography (Resources):
+OpenAI. (2024). ChatGPT [Large language model]. https://chat.openai.com/chat
+This AI device was used to help develop the list of songs used in the songlist.txt file, the use of AI for this aspect of the project is part of the reason for the purpose of the alter function in the program, which allows users to take the list of songs and change some of the assigned emotions around depending on what the user thinks.
+
+w3schools.com 
+This website was a good help and reference when we we're coding as there were some times that we we're stuck and needed a refreshment on things we learned earlier in the semester that we could apply to our code here (such as regular expression)
+
+
+python-engineer.com
+(This falls under the same category as w3schools but python-engineer helped alot with explanations on how to use certain things in python and the examples feel a bit more indepth, so while it was a refresher as well as an opportunity to learn.)
--- Unittest.py
+++ Unittest.py
@@ -3,43 +3,46 @@
 import unittest
 import methodholder
 
+class test(unittest.TestCase):
+    def test_randomize(self):
+        # Call the function and get the returned strings
+        output = methodholder.randomize("randomize", "Ruka")
+        notoutput = methodholder.randomize("randomize", "Hannah")
+        self.assertNotEquals(output, notoutput)
+        # Check if the expected strings are returned
+        
+        #assert ("Hello there!") in output
+        #assert ("Here is a randomized list of 10 songs from the songlist file.") in output
+        #print("successful")
 
-def test_randomize():
-    # Call the function and get the returned strings
-    output = methodholder.randomize("randomize", "Ruka")
 
-    # Check if the expected strings are returned
-    
-    assert ("Hello there!") in output
-    assert ("Here is a randomized list of 10 songs from the songlist file.") in output
-    print("successful")
+    #def test_cyclecontinue(self):
+         #tests the cycle continue which allows users to continue using the program without having to exit.
+         #output = methodholder.cyclecontinue("selection", "Ruka")
+         #self.assertRaises()
+         #assert ("You have the choice to either quit the program ('EXIT'), or continue using the other features available.") in output
+         #print("successful")
 
+    #def test_altertracks():
+         #output = methodholder.altertracks("alter", "Hannah")
+         #assert("Exiting track modification.") in output
+         #print("successful")
 
-def test_cyclecontinue():
-    #tests the cycle continue which allows users to continue using the program without having to exit.
-    output = methodholder.cyclecontinue("selection", "Ruka")
-    
-    assert ("You have the choice to either quit the program ('EXIT'), or continue using the other features available.") in output
-    print("successful")
+    def test_recommendation(self):
+        output = methodholder.recommendation("somethingelse", "Hannah")
+        input = methodholder.recommendation("reommend", "Hannah")
+        self.assertNotEquals(output, input)
+      #if methodholder.recommendation(selection="recommend") is False:
+       #  print("error")
 
-def test_altertracks():
-    output = methodholder.altertracks("alter", "Hannah")
-    assert("Exiting track modification.") in output
-    print("successful")
-
-def test_recommendation():
-    output = methodholder.recommendation("somethingelse", "Hannah")
-    if methodholder.recommendation(selection="recommend") is False:
-        print("error")
+    #     pass
         
 
 if __name__ == '__main__':
- #   test_randomize()
-  #  test_cyclecontinue()
-  #  test_altertracks()
-    test_recommendation()
-
-
-
+    unittest.main()
+    #test_randomize()
+    #  test_cyclecontinue()
+    #  test_altertracks()
+    #test_recommendation()
 
       
